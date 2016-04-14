ac52962c9c731dc43c149c9aaf09b36f56f7b66e
Thorin <c.nethercott@icloud.com>
2016-04-14 11:45:45 +0100

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -4,17 +4,11 @@
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup()
-{
+void setup() {
 	// initialize the LCD
 	lcd.begin();
-
-	// Turn on the blacklight and print a message.
-	lcd.backlight();
+	// prints Hello World
 	lcd.print("Hello, world!");
 }
 
-void loop()
-{
-	// Do nothing here...
-}
+void loop() {}
